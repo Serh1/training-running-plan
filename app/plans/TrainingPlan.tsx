fbb85fe80b56099640aa9490745a91954a30d862
Mateiu <s.mateiu@yahoo.com>
2023-09-11 16:35:02 +0300

    Improving plan generator

--- app/plans/TrainingPlan.tsx
+++ app/plans/TrainingPlan.tsx
@@ -15,30 +15,89 @@ function generatePlan(distance: number, weeks: number, runsPerWeek: string) {
   const runsPerWeekArray = runsPerWeek.split("-");
   const minRunsPerWeek = parseInt(runsPerWeekArray[0]);
   const maxRunsPerWeek = parseInt(runsPerWeekArray[1]);
-  const plan = [];
+  const plan = [] as object[];
 
   let weekDaysCopy = [...weekDays];
   for (let i = 0; i < weeks; i++) {
-    const week = [];
     const max = Math.random() < 0.3 ? minRunsPerWeek : maxRunsPerWeek;
     weekDaysCopy = [...weekDays];
+    const week = [
+      {
+        day: "Monday",
+        type: "Rest",
+        distance: 0,
+      },
+      {
+        day: "Tuesday",
+        type: "Rest",
+        distance: 0,
+      },
+      {
+        day: "Wednesday",
+        type: "Rest",
+        distance: 0,
+      },
+      {
+        day: "Thursday",
+        type: "Rest",
+        distance: 0,
+      },
+      {
+        day: "Friday",
+        type: "Rest",
+        distance: 0,
+      },
+      {
+        day: "Saturday",
+        type: "Rest",
+        distance: 0,
+      },
+      {
+        day: "Sunday",
+        type: "Rest",
+        distance: 0,
+      },
+      {
+        day: "Total",
+        type: "",
+        distance: 0,
+      },
+    ];
 
     for (let j = 0; j < max; j++) {
       const index = Math.floor(Math.random() * weekDaysCopy.length);
       const selectedDay = weekDaysCopy[index];
       weekDaysCopy.splice(index, 1);
-      const run = {
-        day: selectedDay ?? "Other",
-        type: runningTypes[Math.floor(Math.random() * runningTypes.length)],
-        distance: Math.floor(Math.random() * distance + distance / 4),
-      };
-      weekDaysCopy.splice(index, 1);
 
-      week.push(run);
+      const run = {
+        day: selectedDay as string,
+        type: runningTypes[
+          Math.floor(Math.random() * runningTypes.length)
+        ] as string,
+        distance: Math.floor(Math.random() * distance + distance / 4) as number,
+      };
+
+      week.map((day) => {
+        if (day["day"] === selectedDay) {
+          day["type"] = run.type;
+          day["distance"] = run.distance;
+        }
+      });
     }
+
+    let totalDistance = 0;
+    week.map((day) => {
+      if (day["type"] !== "Rest") {
+        totalDistance += day["distance"];
+      }
+    });
+    week[7]["distance"] = totalDistance;
 
     plan.push(week);
   }
+
+  plan[weeks - 1][6].type = "RACE DAY";
+  plan[weeks - 1][6].distance = distance;
 
   plan.map((week) => {
     week.sort((a, b) => {
@@ -62,16 +121,21 @@ function generatePlan(distance: number, weeks: number, runsPerWeek: string) {
         return (
           <ul
             className="
-          bg-slate-100 h-full container mb-2 px-4 
-      flex justify-center items-center p-4 rounded
-          "
+            bg-gray-200 h-full container mb-2 px-4 flex justify-center items-center p-4 rounded-xl"
           >
-            Week #{plan.indexOf(week) + 1} :
+            Week#{plan.indexOf(week) + 1}
             {week.map((run) => {
               return (
-                <li className="flex flex-row-reverse justify-center items-center m-2">
-                  {run.day} {run.type} {run.distance}K,
-                </li>
+                <div className="flex-col  w-full">
+                  <li className="flex flex-row-reverse justify-center items-center m-2">
+                    {run.day}
+                  </li>
+                  <li className="flex flex-row-reverse justify-center items-center m-2">
+                    {run.type === "Rest"
+                      ? run.type
+                      : run.distance + "K " + run.type}
+                  </li>
+                </div>
               );
             })}
           </ul>
